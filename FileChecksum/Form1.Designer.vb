aa7ddd20d6deb80304eef45e15d19798182d75ee
CalebWoolley <calebswoolley@gmail.com>
2015-07-20 18:26:25 -0400

    File Checksum Generator
    Generate checksum for files, drag and drop style.

---
+++ FileChecksum/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(426, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Info: Drag and drop the file that you want to gather checksums for anywhere on th" & _
+    "is form."
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 25)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(448, 60)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Checksums - "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 42)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "SHA256:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "SHA1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "MD5:"
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(468, 90)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "File Checksum Generator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ FileChecksum/Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Imports System.IO
+''' <summary>
+''' Coded by Caleb Woolley, uploaded via GitHub 2015.
+''' </summary>
+''' <remarks></remarks>
+Public Class Form1
+    Dim FilePath As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CheckForIllegalCrossThreadCalls = False
+        Dim FSize As String = GetFileSize(Application.ExecutablePath)
+        GroupBox1.Text = "Checksums - " & Application.ExecutablePath.Substring(Application.ExecutablePath.LastIndexOf("\") + 1, Application.ExecutablePath.Length - (Application.ExecutablePath.LastIndexOf("\") + 1)) & " (" & If(Format(FSize / 1024, "#0.00") = 0, FSize & " bytes", Format(FSize / 1024, "#0.00") & " KB") & ")"
+        Label2.Text = "MD5: " & MD5CalcFile(Application.ExecutablePath)
+        Label3.Text = "SHA1: " & SHA1File(Application.ExecutablePath)
+        Label4.Text = "SHA256: " & SHA256File(Application.ExecutablePath)
+
+        ' Second app
+        Dim doneWithInit As New Threading.EventWaitHandle(False, Threading.EventResetMode.ManualReset, "MyWaitHandle")
+
+        ' Here, the second application initializes what it needs to.
+        ' When it's done, it signals the wait handle:
+        doneWithInit.[Set]()
+    End Sub
+
+    Private Sub Form1_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop
+        On Error Resume Next
+        Dim file_names As String() = DirectCast(e.Data.GetData(DataFormats.FileDrop), String())
+        FilePath = file_names(0)
+        If IsDirectory(FilePath) Then Exit Sub
+        Dim T As New Threading.Thread(AddressOf GatherInfo)
+        T.Start()
+    End Sub
+
+    Private Sub Form1_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+#Region " Threading "
+    Public Sub GatherInfo()
+        Me.Text = "File Checksum Generator"
+        Label2.Text = "MD5: Loading..."
+        Label3.Text = "SHA1: Loading..."
+        Label4.Text = "SHA256: Loading..."
+        Dim FSize As String = GetFileSize(FilePath)
+        GroupBox1.Text = "Checksums - " & FilePath.Substring(FilePath.LastIndexOf("\") + 1, FilePath.Length - (FilePath.LastIndexOf("\") + 1)) & " (" & If(Format(FSize / 1024, "#0.00") = 0, FSize & " bytes", Format(FSize / 1024, "#0.00") & " KB") & ")"
+
+        Dim MD5 As New Threading.Thread(AddressOf MD5Thread)
+        Dim SHA1 As New Threading.Thread(AddressOf SHA1Thread)
+        Dim SHA256 As New Threading.Thread(AddressOf SHA256Thread)
+        MD5.Start()
+        SHA1.Start()
+        SHA256.Start()
+
+        Me.Text = "File Checksum Generator"
+    End Sub
+
+    Public Sub MD5Thread()
+        Label2.Text = "MD5: " & MD5CalcFile(FilePath)
+    End Sub
+    Public Sub SHA1Thread()
+        Label3.Text = "SHA1: " & SHA1File(FilePath)
+    End Sub
+    Public Sub SHA256Thread()
+        Label4.Text = "SHA256: " & SHA256File(FilePath)
+    End Sub
+#End Region
+
+#Region " Copying "
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click, Label3.Click, Label4.Click
+        If MessageBox.Show("Copy this hash?", "Checksum Copying", MessageBoxButtons.YesNo) = vbYes Then
+            Clipboard.SetText(sender.Text.SubString(sender.Text.IndexOf(" ") + 1))
+        End If
+    End Sub
+#End Region
+
+#Region " Functions "
+    Public Function IsDirectory(ByRef Path As String) As Boolean
+        Dim attr As FileAttributes = File.GetAttributes(Path)
+        If (attr And FileAttributes.Directory) = FileAttributes.Directory Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Function MD5CalcFile(ByVal filepath As String) As String
+        Using reader As New System.IO.FileStream(filepath, IO.FileMode.Open, IO.FileAccess.Read)
+            Using md5 As New System.Security.Cryptography.MD5CryptoServiceProvider
+                Dim hash() As Byte = md5.ComputeHash(reader)
+                Return ByteArrayToString(hash)
+            End Using
+        End Using
+    End Function
+    Public Function SHA1File(ByRef Path As String) As String
+        Dim SHA1 As New SHA1CryptoServiceProvider()
+        Dim myfile As Byte() = File.ReadAllBytes(Path)
+        Return ByteArrayToString(SHA1.ComputeHash(myfile))
+    End Function
+    Public Function SHA256File(ByRef Path As String) As String
+        Dim sha As New SHA256Managed()
+        Dim myfile As Byte() = File.ReadAllBytes(Path)
+        Return ByteArrayToString(sha.ComputeHash(myfile))
+    End Function
+
+    Private Function ByteArrayToString(ByVal arrInput() As Byte) As String
+        Dim sb As New System.Text.StringBuilder(arrInput.Length * 2)
+        For i As Integer = 0 To arrInput.Length - 1
+            sb.Append(arrInput(i).ToString("X2"))
+        Next
+        Return sb.ToString().ToLower
+    End Function
+
+    Private Function GetFileSize(ByVal MyFilePath As String) As Long
+        Dim MyFile As New FileInfo(MyFilePath)
+        Dim FileSize As Long = MyFile.Length
+        Return FileSize
+    End Function
+#End Region
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
+
+
